cda53398d9950a69874ad9b91d75327630ef1e5e
snedjace <snedjace@uw.edu>
2025-03-10 19:05:08 -0700

    Added a legend to the map.

--- js/script.js
+++ js/script.js
@@ -3,11 +3,20 @@ const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
     center: [-122.335167, 47.62],
-    zoom: 9.8
+    zoom: 10.5
 });
 
 const square = document.querySelector('input[value="square"]');
 const nominative = document.querySelector('input[value="nominative"]');
+
+const colors = [
+    '#999999',
+    '#FEE7E7',
+    '#E9A698',
+    '#C9654F',
+    '#A2160B'
+];
+
 
 function addNewLayer(layerName, source, variable, num1, num2, num3) {
     map.addLayer({
@@ -19,18 +28,59 @@ function addNewLayer(layerName, source, variable, num1, num2, num3) {
             'fill-color': [
                 'step',
                 ['get', variable],
-                '#999999',
+                colors[0],
                 1,
-                '#FEE7E7', 
+                colors[1], 
                 num1,
-                '#E9A698',
+                colors[2],
                 num2,
-                '#C9654F',
+                colors[3],
                 num3,
-                '#A2160B'
+                colors[4]
             ],
             'fill-outline-color': "#6E6E6E"
         }
+    });
+
+    let title; 
+    let form1;
+    let form2;
+    let form3;
+
+    if (variable === "PRICE_SQFT") {
+        title = "per square foot";
+        form1 = new Intl.NumberFormat('en-US').format(num1 - 0.001);
+        form2 = new Intl.NumberFormat('en-US').format(num2 - 0.001);
+        form3 = new Intl.NumberFormat('en-US').format(num3 - 0.001);
+    } else {
+        title = "nominative";
+        form1 = new Intl.NumberFormat('en-US').format(num1 - 1);
+        form2 = new Intl.NumberFormat('en-US').format(num2 - 1);
+        form3 = new Intl.NumberFormat('en-US').format(num3 - 1);
+    }
+
+    const layers = [
+        "No Data",
+        "≤ $" + form1,
+        "$" + form1 + " to $" + form2,
+        "$" + form2 + " to $" + form3,
+        "> $" + form3
+    ];
+
+    const legend = document.getElementById('legend');
+    legend.innerHTML = "<b>Price </b>(" + title + ")<br>";
+
+    layers.forEach((layer, i) => {
+        const color = colors[i];
+        const item = document.createElement('div');
+        const key = document.createElement('span');
+        key.className = 'legend-key';
+        key.style.backgroundColor = color;
+        const value = document.createElement('span');
+        value.innerHTML = `${layer}`;
+        item.appendChild(key);
+        item.appendChild(value);
+        legend.appendChild(item);
     });
 }
 
